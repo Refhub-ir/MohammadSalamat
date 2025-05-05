0257285fff0038451bcf847fededfa92aec5aa98
mohammad-81 <mmdsallamat2002@gmail.com>
2025-05-05 22:52:19 +0330

    Add (Backend - ASP.NET Core MVC)

---
+++ RefhubMVC/DB_context/Refhub-db.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RefhubMVC.DB_context
+{
+    public class Refhub_db : DbContext
+    {
+        public Refhub_db(DbContextOptions<Refhub_db> options) : base(options) { }
+    }
+}
--- RefhubMVC/Program.cs
+++ RefhubMVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RefhubMVC.DB_context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Refhub_db>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
